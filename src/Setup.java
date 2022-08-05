8b99e2e17eefe70b009cf8b1683b97932f325da1
Vicente Cruz <vicentecruzf@gmail.com>
2022-08-04 21:08:54 -0400

    cree otro personaje

--- src/Setup.java
+++ src/Setup.java
@@ -1,6 +1,8 @@
 public class Setup {
     public static final int [] ataques = new int []{1, 2, 3, 4};
+    public static final int [] ataques2 = new int []{5, 6, 7, 8};
     
     public static final Personaje perso1 = new Personaje(1 , "sawano", ataques , 1500);
+    public static final Personaje perso2 = new Personaje(2 , "Fede", ataques2 , 1500);
     
 }
